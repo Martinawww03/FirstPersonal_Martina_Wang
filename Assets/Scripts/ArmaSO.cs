07ea35c467b6d1909a2feafb868605ac8b99789f
Martinawww03 <martinawang03@gmail.com>
2024-11-06 17:11:20 +0100

    luces

---
+++ Assets/Scripts/ArmaSO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName ="Arma")]
+public class ArmaSO : ScriptableObject
+{
+    public int balasCargador;
+    public int balasBolsa;
+    public float cadenciaAtaque; //rapidez de ataque
+    public float distanciaAtaque;
+    public float danhoAtaque;
+}
