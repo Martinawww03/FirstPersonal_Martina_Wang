d83a29b679a9c21e66e476b0f6171fa03886bc30
Martinawww03 <martinawang03@gmail.com>
2025-01-11 13:31:24 +0100

    Menu de start

--- Assets/Scripts/Start.cs
+++ Assets/Scripts/Start.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Start : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+        Application.Quit();
+    }
+    public void VolverAMenu()
+    {
+        //SceneManeger.LoadScene(0);
     }
 }
