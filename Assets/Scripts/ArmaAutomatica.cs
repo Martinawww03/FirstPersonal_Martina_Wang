1615d8cf501cc2a0325d4bc907060bec0a0bb36c
DESKTOP-GV16N45\Alumno <martinawang03@gmail.com>
2025-01-15 17:19:18 +0100

    Proyecto final

--- Assets/Scripts/ArmaAutomatica.cs
+++ Assets/Scripts/ArmaAutomatica.cs
@@ -6,27 +6,33 @@ public class ArmaAutomatica : MonoBehaviour
 {
     [SerializeField] private ParticleSystem system;
     [SerializeField] private ArmaSO misDatos;
-    private float timer;
     private Camera cam;
+    private float timer;
 
     private void Start()
     {
         cam = Camera.main;
+
         timer = misDatos.cadenciaAtaque;
     }
     private void Update()
     {
         timer += Time.deltaTime;
+
         if(Input.GetMouseButton(0)&& timer >= misDatos.cadenciaAtaque)
         {
             system.Play();
-            if(Physics.Raycast(cam.transform.position,cam.transform.forward,out RaycastHit hit, misDatos.distanciaAtaque)) //Hace una raya hasta enfrente
+            if(Physics.Raycast(cam.transform.position,cam.transform.forward,out RaycastHit hitInfo, misDatos.distanciaAtaque)) //Hace una raya hasta enfrente
             {
-                system.Play();
-                if(hit.transform.CompareTag("ParteEnemigo"))
+                if(hitInfo.transform.TryGetComponent(out ParteDeEnemigo scriptEnemigo))
                 {
-                    hit.transform.GetComponent<ParteDeEnemigo>().RecibirDanno(misDatos.distanciaAtaque);
+                    scriptEnemigo.RecibirDanno(misDatos.danhoAtaque);
                 }
+                //system.Play();
+                //if(hit.transform.CompareTag("ParteEnemigo"))
+                //{
+                //    hit.transform.GetComponent<ParteDeEnemigo>().RecibirDanno(misDatos.distanciaAtaque);
+                //}
             }
             timer = 0;
         }
--- Assets/Scripts/ArmaSO.cs
+++ Assets/Scripts/ArmaSO.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ArmaSO : MonoBehaviour
+[CreateAssetMenu(menuName ="Arma2")]
+public class ArmaSO : ScriptableObject
 {
     public int balasCargador;
     public int balasBolsa;
