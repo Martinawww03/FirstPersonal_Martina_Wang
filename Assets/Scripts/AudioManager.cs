6c1e5c5cdb5d2798b54a2525f717f7f4980f0b5a
DESKTOP-GV16N45\Alumno <martinawang03@gmail.com>
2025-01-15 16:09:31 +0100

    Quitar sonido

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
-// AudioManager= yo
-{
-    [SerializeField] private AudioSource audioSourceSfx; //AudioSource= Spoty
-
-    public void ReproducirSonido(AudioClip clip) //clip= canción
-    {
-        //Reproducir una vez el sonido.
-        audioSourceSfx.PlayOneShot(clip);
-    }
-
-}
+//public class AudioManager : MonoBehaviour
+//// AudioManager= yo
+//{
+//    [SerializeField] private AudioSource audioSourceSfx; //AudioSource= Spoty
+//
+//    public void ReproducirSonido(AudioClip clip) //clip= canción
+//    {
+//        //Reproducir una vez el sonido.
+//        audioSourceSfx.PlayOneShot(clip);
+//    }
+//
+//}
